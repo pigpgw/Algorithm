a5510455dcb81586fa65c2a684d733d478d1bce0
pigpgw <133184988+pigpgw@users.noreply.github.com>
2024-07-10 19:38:50 +0900

    [Gold IV] Title: 사냥꾼, Time: 460 ms, Memory: 57936 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,3 +1,4 @@
+
 import sys
 input = sys.stdin.readline
 
@@ -9,30 +10,30 @@ M_list.sort()
 animals = [list(map(int,input().split())) for _ in range(N)]
 
 
-# print('M',M,'N',N)
-# print('',L,)
-# print('M_list\n',M_list)
-# print('animals',animals)
 
 def count_animal(sadae_list,animal_list,gun_range):
-    left = 1
-    right = len(sadae_list) - 1
     전체_죽은_고라니 = 0
 
     for 고라니 in animal_list:
-        mid = (left + right) // 2
-        고라니죽었니=False
-        for 사대 in sadae_list:
-            고라니가_죽을수_있는_범위 = abs(사대 - 고라니[0]) + 고라니[1]
-            if 고라니가_죽을수_있는_범위 <= gun_range:
+        고라니_x,고라니_y = 고라니
+        left = 0 
+        right = len(sadae_list) - 1
+        고라니죽었니 = False
+
+        while left <= right:
+            mid = (left + right) // 2
+            사대 = sadae_list[mid]
+            고라니_죽는_범위 = abs(사대 - 고라니_x) + 고라니_y
+
+            if 고라니_죽는_범위 <= gun_range:
                 고라니죽었니 = True
                 break
-
+            elif 사대 < 고라니_x:
+                left = mid + 1
+            else:
+                right = mid - 1
         if 고라니죽었니:
             전체_죽은_고라니 += 1
     return 전체_죽은_고라니
-    
-    # 고라니를 기준으로 사로를 다 체크해 업데이트 근데 이진탐색?
-                
 
 print(count_animal(M_list,animals,L))
