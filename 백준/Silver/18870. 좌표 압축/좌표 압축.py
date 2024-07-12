d3ba6cc8772b89c0da96329e7ebd7a33f715f9b2
pigpgw <133184988+pigpgw@users.noreply.github.com>
2024-07-12 16:36:37 +0900

    [Silver II] Title: 좌표 압축, Time: 1944 ms, Memory: 144340 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(sys.stdin.readline().strip())
+
+X = list(map(int, sys.stdin.readline().strip().split()))
+
+X_sort = sorted(list(set(X)))
+
+dic = {}
+for i in range(len(X_sort)):
+  dic[X_sort[i]] = i
+
+for i in X:
+  print(dic[i], end=" ")
