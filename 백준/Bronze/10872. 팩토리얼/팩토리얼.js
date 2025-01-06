1f94a68bdabb554e23c0fb9a2cc6dcd991c96eb5
pigpgw <133184988+pigpgw@users.noreply.github.com>
2025-01-07 03:07:48 +0900

    [Bronze III] Title: 팩토리얼, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.js
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -1,7 +1,7 @@
 const input = require('fs').readFileSync(0, 'utf-8').trim();
+const factorial = (x) => {
+    if (x <= 1) return 1;
+    else return (x *= factorial(x - 1));
+};
 
-let factorial = 1;
-for (let i = 1; i <= input; i++) {
-    factorial *= i;
-}
-console.log(factorial);
+console.log(factorial(Number(input)));
