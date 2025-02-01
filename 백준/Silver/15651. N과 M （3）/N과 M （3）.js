67b56d67313b0969de029fe067c5d9ec50b23abe
pigpgw <133184988+pigpgw@users.noreply.github.com>
2025-02-01 15:44:58 +0900

    [Silver III] Title: N과 M (3), Time: 744 ms, Memory: 138660 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -0,0 +1,21 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+const [n, m] = input.split(' ').map(Number);
+let result = '';
+const seq = [];
+
+const solution = (count) => {
+    if (count === m) {
+        result += `${seq.join(' ')}\n`;
+        return;
+    }
+
+    for (let i = 1; i <= n; i++) {
+        seq.push(i);
+        solution(count + 1);
+        seq.pop();
+    }
+};
+
+solution(0);
+console.log(result);
