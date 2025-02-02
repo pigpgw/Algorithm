0a6839243d8ea14b1668f218cfe47be80bd25c5e
pigpgw <133184988+pigpgw@users.noreply.github.com>
2025-02-02 16:44:46 +0900

    [Silver II] Title: N과 M (12), Time: 168 ms, Memory: 12204 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -0,0 +1,26 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const numberList = input[1]
+    .split(' ')
+    .map(Number)
+    .sort((a, b) => a - b);
+
+const seq = [];
+let result = [];
+
+const dfs = (count) => {
+    if (count === m) {
+        result.push(seq.join(' '));
+        return;
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (Math.max(...seq) > numberList[i]) continue;
+        seq.push(numberList[i]);
+        dfs(count + 1);
+        seq.pop();
+    }
+};
+dfs(0);
+console.log([...new Set(result)].join('\n'));
