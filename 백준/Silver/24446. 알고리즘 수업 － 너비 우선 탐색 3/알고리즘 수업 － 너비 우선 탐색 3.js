58b3d2cabf93e2ef9219dfb78a1f0df64eed6d5f
pigpgw <133184988+pigpgw@users.noreply.github.com>
2025-02-09 21:42:23 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 3, Time: 744 ms, Memory: 109152 KB -BaekjoonHub

---
+++ 백준/Silver/24446. 알고리즘 수업 － 너비 우선 탐색 3/알고리즘 수업 － 너비 우선 탐색 3.js
@@ -0,0 +1,35 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [N, M, R] = input[0].split(' ').map(Number);
+const trunkList = input.slice(1).map((item) => item.split(' ').map(Number));
+
+const graph = Array.from({ length: N + 1 }, () => []);
+const visited = Array(N + 1).fill(-1);
+
+trunkList.forEach(([a, b]) => {
+    graph[a].push(b);
+    graph[b].push(a);
+});
+
+graph.forEach((item) => {
+    item.sort((a, b) => a - b);
+});
+
+const bfs = (start) => {
+    const queue = [];
+    let front = 0;
+    queue.push([start, 0]);
+    visited[start] = 0;
+    while (queue.length > front) {
+        const [cur, depth] = queue[front++];
+        for (const next of graph[cur]) {
+            if (visited[next] === -1) {
+                visited[next] = depth + 1;
+                queue.push([next, depth + 1]);
+            }
+        }
+    }
+};
+
+bfs(R);
+console.log(visited.slice(1).join('\n'));
