aa71d1303ec308c121fd16e0abd65ac4e25e9d4b
pigpgw <133184988+pigpgw@users.noreply.github.com>
2025-05-18 14:40:17 +0900

    [level 2] Title: 피로도, Time: 65.04 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,24 +1,15 @@
 def solution(k, dungeons):
     n = len(dungeons)
     max_count = 0
-    def backtrack(fatigue,curr,depth):
+
+    def dfs(current_k, used_mask, count):
         nonlocal max_count
-        # 모든경우를 탐색하며 각 경우별 가능한 일일 최대 던전 탐험 개수를 갱신한다.
-        # 기저 조건 던전을 다 돌았을 경우
-        # 최소 필요 피로도
-        # 백트랙으로 한다
-        if depth > max_count:
-            max_count = depth
-        
-        for dungeon_info in dungeons:
-            if dungeon_info in curr:
-                continue
-            min_needs_fatigue,consume_fatigue = dungeon_info
-            if fatigue >= min_needs_fatigue:
-                curr.append(dungeon_info)
-                backtrack(fatigue - consume_fatigue,curr,depth + 1)
-                curr.pop()
-            else:
-                continue
-    backtrack(k,[],0)
+        max_count = max(max_count, count)
+        for i in range(n):
+            if not (used_mask & (1 << i)):
+                min_req, cost = dungeons[i]
+                if current_k >= min_req:
+                    dfs(current_k - cost, used_mask | (1 << i), count + 1)
+
+    dfs(k, 0, 0)
     return max_count
