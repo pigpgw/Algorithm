daa2d075e981f9ccfaf071ddb0fdc5a6c94fd7e1
pigpgw <133184988+pigpgw@users.noreply.github.com>
2025-05-18 14:56:57 +0900

    [level 2] Title: 피로도, Time: 35.77 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,15 +1,14 @@
+from  itertools import permutations
 def solution(k, dungeons):
+    answer = -1
     n = len(dungeons)
-    max_count = 0
-
-    def dfs(current_k, used_mask, count):
-        nonlocal max_count
-        max_count = max(max_count, count)
-        for i in range(n):
-            if not (used_mask & (1 << i)):
-                min_req, cost = dungeons[i]
-                if current_k >= min_req:
-                    dfs(current_k - cost, used_mask | (1 << i), count + 1)
-
-    dfs(k, 0, 0)
-    return max_count
+    for order in permutations(range(n)):
+        cur = k
+        local_ans = 0
+        for t in order:
+            require,consume = dungeons[t]
+            if cur >= require:
+                cur -= consume
+                local_ans += 1
+        answer = max(answer,local_ans)
+    return answer
