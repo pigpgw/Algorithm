3f9baa816e2a03e3efde62423f1eda91b0271851
pigpgw <133184988+pigpgw@users.noreply.github.com>
2025-05-28 12:49:30 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 588 ms, Memory: 112968 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,22 +1,36 @@
 import math
 
-def isPrime(num):
-    if num == 1:
-        return
-    for i in range(2,int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True                    
+def isPrime(number):
+    is_prmise = [True] * (number + 1)
+    is_prmise[0] = is_prmise[1] = False
+    for i in range(2, int(number ** 0.5) + 1):
+        if is_prmise[i]:
+            for j in range(i * i, number + 1, i):
+                is_prmise[j] = False
+    return is_prmise
 
-number = int(input())
-for _ in range(number):
-    input_num = int(input())
-    min_prime = input_num // 2
-    max_prime = input_num // 2  
-    for i in range(input_num // 2):
-        if isPrime(min_prime) and isPrime(max_prime):
-            print(min_prime,max_prime)
-            break
-        else:
-            min_prime -= 1
-            max_prime += 1
+T = int(input())
+
+for _ in range(T):
+    target_number = int(input())
+    is_prmise = isPrime(target_number)
+    result = []
+
+    for i in range(2, target_number // 2 + 1):
+        j = target_number - i
+        if is_prmise[i] and is_prmise[j]:
+            result.append((i, j))
+
+    if not result:
+        continue  # 아무 쌍도 없을 경우
+    elif len(result) == 1:
+        print(result[0][0], result[0][1])
+    else:
+        min_gap = math.inf
+        index = -1
+        for i in range(len(result)):
+            gap = result[i][1] - result[i][0]
+            if gap < min_gap:
+                min_gap = gap
+                index = i
+        print(result[index][0], result[index][1])
