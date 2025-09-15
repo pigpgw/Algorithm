937b3471440e413aa8531e65ec728b572ee6b272
pigpgw <133184988+pigpgw@users.noreply.github.com>
2025-09-15 23:44:32 +0900

    [Silver II] Title: 쇠막대기, Time: 188 ms, Memory: 13288 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('');
+const stack = [];
+let result = 0;
+for (let i = 0; i < input.length; i++) {
+    if (input[i] === '(') stack.push('(');
+    else {
+        stack.pop();
+        if (input[i - 1] === '(') {
+            result += stack.length;
+        } else {
+            result += 1;
+        }
+    }
+}
+console.log(result);
