42dffadab572ea79eab62d2d467891e5687a5e05
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-01-02 00:24:03 +0900

    [Bronze V] Title: 나머지, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+let [A, B, C] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
