46ab93d15c7c81b8e0e45973f701374ab8ab1ef7
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-01-05 22:21:33 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 1288 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.js
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.js
@@ -1,39 +1,25 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
-
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
 const T = input[0];
 const input_list = input.slice(1);
-
-const generate_prime_array = (n) => {
-    const array = [];
-    for (let i = 2; i < n; i++) {
-        let isPrime = true;
-        for (let j = 2; j * j <= i; j++) {
-            if (i % j === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {
-            array.push(i);
-        }
+const isDecimal = (number) => {
+    for (let i = 2; i * i <= Math.floor(number); i++) {
+        if (number % i === 0) return false;
     }
-    return array;
+    return true;
 };
 
-for (const n of input_list) {
-    const sieve = new Array(n).fill(false);
-    const prime_array = generate_prime_array(n);
-    for (const prime of prime_array) {
-        sieve[prime] = true;
-    }
-
-    let answer = null;
-    for (let i = Math.floor(n / 2); i >= 2; i--) {
-        if (sieve[i] && sieve[n - i]) {
-            answer = [i, n - i];
-            break;
+for (const num of input_list) {
+    const thieve = [];
+    for (let i = 2; i <= Math.floor(num / 2); i++) {
+        if (isDecimal(i) && isDecimal(num - i)) {
+            thieve.push([i, num - i]);
         }
     }
-    console.log(answer[0], answer[1]);
+    console.log(
+        thieve
+            .sort((a, b) => a[0] - b[0])
+            [thieve.length - 1].map(Number)
+            .join(' ')
+    );
 }
