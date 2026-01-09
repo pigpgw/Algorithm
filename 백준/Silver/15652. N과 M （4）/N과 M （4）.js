192b85c10909c754f090ac51101da95d2a7f344f
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-01-09 21:26:42 +0900

    [Silver III] Title: N과 M (4), Time: 160 ms, Memory: 12136 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.js
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -1,22 +1,21 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+const fs = require('fs');
+const [N, M] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 
-const [n, m] = input.split(' ').map(Number);
-let result = '';
-const seq = [];
+const arr = [];
+let output = '';
 
-const solution = (count) => {
-    if (count === m) {
-        result += `${seq.join(' ')}\n`;
+const dfs = (depth, curr) => {
+    if (depth === M) {
+        output += arr.join(' ') + '\n';
         return;
     }
 
-    for (let i = 1; i <= n; i++) {
-        if (Math.max(...seq) > i) continue;
-        seq.push(i);
-        solution(count + 1);
-        seq.pop();
+    for (let i = curr; i <= N; i++) {
+        arr.push(i);
+        dfs(depth + 1, i);
+        arr.pop();
     }
 };
 
-solution(0);
-console.log(result);
+dfs(0, 1);
+console.log(output);
