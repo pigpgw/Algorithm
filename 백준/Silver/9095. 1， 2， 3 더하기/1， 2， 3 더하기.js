c04db4e32f6a3430886dc152653f62f307c3d8b3
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-01-09 11:37:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 88 ms, Memory: 9364 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
-let input_list = input.slice(1);
-
-const dp = new Array(10 + 1).fill(0);
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const T = Number(input[0]);
+const n_llist = input.slice(1).map(Number);
+const dp = new Array(11).fill(0);
 dp[1] = 1;
 dp[2] = 2;
 dp[3] = 4;
-
-for (let i = 4; i <= 10; i++) {
+for (let i = 4; i < 11; i++) {
     dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
 }
-for (const num of input_list) {
-    console.log(dp[num]);
+for (const n of n_llist) {
+    console.log(dp[n]);
 }
