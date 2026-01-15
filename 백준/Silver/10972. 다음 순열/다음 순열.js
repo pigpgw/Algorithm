5db1970d9224da22933bdf91f162c1b8a0505707
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-01-15 23:37:16 +0900

    [Silver III] Title: 다음 순열, Time: 104 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0]);
+const target = input[1].split(' ').map(Number);
+
+let index = target.length - 1;
+let store = [];
+
+while (index > 0) {
+    store.push(target[index]);
+    const nextIndex = index - 1;
+    const currentValue = target[index];
+    const nextValue = target[nextIndex];
+
+    if (nextValue < currentValue) {
+        // 🔥 핵심 수정
+        const bigger = store.filter((v) => v > nextValue);
+        const minBigger = Math.min(...bigger);
+
+        // 오른쪽 구간에서만 인덱스 찾기
+        const swapIndex = target.lastIndexOf(minBigger);
+
+        [target[nextIndex], target[swapIndex]] = [target[swapIndex], target[nextIndex]];
+        // 오른쪽 정렬
+        const right = target.slice(nextIndex + 1).sort((a, b) => a - b);
+        const result = [...target.slice(0, nextIndex + 1), ...right];
+        console.log(result.join(' '));
+        return;
+    }
+
+    index--;
+}
+
+console.log(-1);
