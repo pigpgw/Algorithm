a53713ee4188da7941b1c7c30e62d4c5d5a30d05
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-01-22 09:42:41 +0900

    [Silver III] Title: 2×n 타일링, Time: 152 ms, Memory: 9680 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim();
-let X = Number(input);
+const N = Number(fs.readFileSync('/dev/stdin').toString().trim());
 
-const dp = new Array(X + 1).fill(0);
+const dp = Array(N + 1).fill(Infinity);
+
+dp[0] = 0;
 dp[1] = 1;
 dp[2] = 2;
 
-for (let i = 3; i <= X; i++) {
+for (let i = 3; i <= N; i++) {
     dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
 }
 
-console.log(dp[X]);
+console.log(dp[N]);
