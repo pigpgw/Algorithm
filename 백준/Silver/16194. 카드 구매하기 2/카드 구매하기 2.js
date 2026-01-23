5e4f676295f873a80b058c865b8ee86c46f54145
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-01-23 22:15:21 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 164 ms, Memory: 10708 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.js
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.js
@@ -1,15 +1,15 @@
+// measure.js
 const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const N = Number(input[0]);
-const card_list = input[1].split(' ').map(Number);
-card_list.unshift(0);
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
 
-const dp = new Array(N + 1).fill(99999);
+// 입력: 첫 줄 N, 둘째 줄 N개의 가격
+const N = Number(input[0]);
+const cost_list = [0, ...input[1].split(' ').map(Number)];
+const dp = Array(N + 1).fill(Infinity);
 dp[0] = 0;
-dp[1] = card_list[1];
 for (let i = 1; i <= N; i++) {
     for (let j = 1; j <= i; j++) {
-        dp[i] = Math.min(dp[i], dp[i - j] + card_list[j]);
+        dp[i] = Math.min(dp[i], dp[i - j] + cost_list[j]);
     }
 }
 
