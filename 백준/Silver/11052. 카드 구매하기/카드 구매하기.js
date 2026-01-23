3f94dcb061588d5fa1d0c7e381b9bf30d0360da1
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-01-23 10:30:32 +0900

    [Silver I] Title: 카드 구매하기, Time: 156 ms, Memory: 9820 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
@@ -5,10 +5,11 @@ const N = Number(input[0]);
 const cost_list = [0, ...input[1].split(' ').map(Number)];
 const dp = Array(N + 1).fill(0);
 
-dp[1] = cost_list[0];
 for (let i = 1; i <= N; i++) {
-    for (let j = 1; j <= i; j++) {
-        dp[i] = Math.max(dp[i - j] + cost_list[j], dp[i]);
+    dp[i] = cost_list[i];
+    for (let j = 1; j <= Math.floor(i / 2); j++) {
+        const candidate = dp[i - j] + cost_list[j];
+        if (candidate > dp[i]) dp[i] = candidate;
     }
 }
 
