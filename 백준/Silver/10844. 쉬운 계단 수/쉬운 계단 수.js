607d730b66bf3069101a820c43bf0d47f2662447
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-01-26 10:07:37 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 100 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.js
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.js
@@ -1,27 +1,18 @@
 const fs = require('fs');
-let input = Number(fs.readFileSync('/dev/stdin').toString().trim());
+const input = Number(fs.readFileSync(0, 'utf8').trim());
 
-const MOD = 1000000000;
-const dp = Array.from({ length: input + 1 }, () => new Array(10).fill(0));
+const MOD = 1_000_000_000;
 
-// 길이가 1일 때 (0으로 시작할 수 없음)
-for (let i = 1; i < 10; i++) {
-  dp[1][i] = 1;
-}
+const dp = Array.from({ length: input + 1 }, () => Array(10).fill(0));
 
-// 점화식 적용
+for (let j = 1; j <= 9; j++) dp[1][j] = 1;
+
 for (let i = 2; i <= input; i++) {
-  for (let j = 0; j < 10; j++) {
-    if (j === 0) {
-      dp[i][j] = dp[i - 1][1] % MOD;
-    } else if (j === 9) {
-      dp[i][j] = dp[i - 1][8] % MOD;
-    } else {
-      dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+    dp[i][0] = dp[i - 1][1] % MOD;
+    for (let j = 1; j <= 8; j++) {
+        dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
     }
-  }
+    dp[i][9] = dp[i - 1][8] % MOD;
 }
 
-// 마지막 줄 합산
-const result = dp[input].reduce((acc, cur) => (acc + cur) % MOD, 0);
-console.log(result);
+console.log(dp[input].reduce((acc, cur) => (acc + cur) % MOD, 0));
