af4050ff88feddc4bf88c8318d42978927067f28
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-01-31 15:26:54 +0900

    [Silver IV] Title: 덱, Time: 452 ms, Memory: 16284 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.js
+++ 백준/Silver/10866. 덱/덱.js
@@ -1,42 +1,43 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-
-const N = input[0];
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+const n = Number(input[0]);
 const order_list = input.slice(1);
 
-const dec = [];
+// 선입 선출 fifo 자료구조
+// 배열 끝에 요소를 추가하면 push 처음에 추가하려면 unshfit
+// 배열 맨 앞 값 제거 shift 배열 맨뒤 Pop
+const deck = [];
 
-const result = [];
-for (const order_content of order_list) {
-    const [order, num] = order_content.split(' ');
+for (const item of order_list) {
+    const l = item.split(' ');
+    let order = l[0];
+    let target = l.length === 2 ? l[1] : undefined;
     switch (order) {
         case 'push_front':
-            dec.unshift(num);
+            deck.unshift(target);
             break;
         case 'push_back':
-            dec.push(num);
+            deck.push(target);
             break;
         case 'pop_front':
-            if (dec.length > 0) result.push(dec.shift());
-            else result.push(-1);
+            console.log(deck.length >= 1 ? deck.shift() : -1);
             break;
         case 'pop_back':
-            if (dec.length > 0) result.push(dec.pop());
-            else result.push(-1);
+            console.log(deck.length >= 1 ? deck.pop() : -1);
             break;
         case 'size':
-            result.push(dec.length);
+            console.log(deck.length);
             break;
         case 'empty':
-            result.push(dec.length === 0 ? 1 : 0);
+            console.log(deck.length === 0 ? 1 : 0);
             break;
         case 'front':
-            result.push(dec.length === 0 ? -1 : dec[0]);
+            console.log(deck.length >= 1 ? deck[0] : -1);
             break;
         case 'back':
-            result.push(dec.length === 0 ? -1 : dec.at(-1));
+            console.log(deck.length >= 1 ? deck[deck.length - 1] : -1);
+            break;
+        default:
             break;
     }
 }
-
-console.log(result.join('\n'));
