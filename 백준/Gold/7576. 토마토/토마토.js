69e32e38815cc43707ce59569400dcfd438d0331
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-02-01 13:53:33 +0900

    [Gold V] Title: 토마토, Time: 568 ms, Memory: 168544 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.js
+++ 백준/Gold/7576. 토마토/토마토.js
@@ -1,50 +1,54 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-
-const [M, N] = input[0].split(' ').map(Number);
-const tomatoList = input.slice(1).map((item) => item.split(' ').map(Number));
-
+const fs = require('fs')
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+const [M, N] = input[0].split(" ").map(Number)
 const visited = Array.from({ length: N }, () => Array(M).fill(false));
-const graph = Array.from({ length: N }, () => Array(M).fill([]));
+const board = input.slice(1).map(item => item.split(" ").map(Number))
 
-const queue = [];
+const dx = [0, -1, 0, 1]
+const dy = [-1, 0, 1, 0]
 
-for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-        if (tomatoList[i][j] === 1) {
-            queue.push([i, j, 0]);
+const queue = []
+let result = 1;
+let head = 0
+
+for (let j = 0; j < N; j++) {
+    for (let i = 0; i < M; i++) {
+        if (board[j][i] === 1) {
+            queue.push([j, i, 1])
+            visited[j][i] = true
         }
     }
 }
-const dx = [0, 1, 0, -1];
-const dy = [1, 0, -1, 0];
-let pointer = 0;
-const dfs = () => {
-    while (queue.length > pointer) {
-        const [x, y, today] = queue[pointer++];
-        graph[x][y] = [x, y, today];
-        for (let i = 0; i <= 3; i++) {
-            const xlocation = x + dx[i];
-            const ylocation = y + dy[i];
-
-            if (xlocation < 0 || xlocation >= N || ylocation < 0 || ylocation >= M) continue;
-            if (visited[xlocation][ylocation] || tomatoList[xlocation][ylocation] !== 0) continue;
-            visited[xlocation][ylocation] = true;
-            queue.push([xlocation, ylocation, today + 1]);
-        }
-    }
-};
-dfs();
-let day = 0;
-let rest = 0;
-
-for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-        const [x, y, recordeDay] = graph[i][j];
-        if (day < recordeDay) day = recordeDay;
-        if (tomatoList[i][j] === 0 && !visited[i][j]) {
-            rest++;
+const bfs = () => {
+    while (head < queue.length) {
+        const [y, x, day] = queue[head++]
+        for (let i = 0; i < 4; i++) {
+            const next_x = x + dx[i]
+            const next_y = y + dy[i]
+            if (
+                next_x < 0 || next_x >= M ||
+                next_y < 0 || next_y >= N
+            ) continue
+            if (board[next_y][next_x] === -1) continue
+            if (visited[next_y][next_x]) continue
+            queue.push([next_y, next_x, day + 1])
+            visited[next_y][next_x] = true
+            board[next_y][next_x] = day + 1 
+            result = Math.max(result,day + 1)
         }
     }
 }
-if (rest === 0) console.log(day);
-else console.log(-1);
+
+bfs()
+
+
+for (let j = 0; j < N; j++) {
+    for (let i = 0; i < M; i++) {
+        if (board[j][i] === 0 ) {
+            console.log(-1)
+            return
+        }
+    }
+}
+
+console.log(result - 1)
