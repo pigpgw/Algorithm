85808c2d5f20e264e1013ccb2c05e05d8dd755ba
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-03-05 13:17:42 +0900

    [level 2] Title: 주식가격, Time: 6.04 ms, Memory: 44.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,17 @@
+function solution(prices) {
+    // 값 저장할것 필요
+    const result = []
+    let head = 0
+    while (prices.length > head){
+        // 앞에서 뽑는다.
+        const poped = prices[head++]
+        // 이후 가격들을 순차적으로 확인한다.(남은 배열개수만큼 근데 떨어지면 종료)
+        let count = 0
+        for (let i = head; i < prices.length; i++){
+            if (prices.length > 0) count++
+            if (poped > prices[i]) break
+        }
+        result.push(count)
+    }
+    return result
+}
