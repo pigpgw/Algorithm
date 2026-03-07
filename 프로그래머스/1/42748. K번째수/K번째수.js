abde5c0f4f9e3e14a504c1e3d62a36c574fb0055
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-03-07 23:56:06 +0900

    [level 1] Title: K번째수, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,6 @@
 function solution(array, commands) {
-    const result = []
-    for (const [i,j,k] of commands){
-        let item_list = i === j ? array.slice(i-1,i) : array.slice(i - 1,j)
-        item_list = item_list.sort((a,b) => a- b)
-        console.log(item_list)
-        result.push(item_list[k - 1])
-    }
-    return result
+    return commands.map(command => {
+        const [startIdx,endIdx,targetIdx] = command
+        return array.filter((item,idx) => idx >= startIdx - 1 && idx <= endIdx - 1).sort((a,b) => a - b)[targetIdx - 1]
+    })
 }
