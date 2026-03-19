b342062c2af7722f0825aabd1c766ca76e3ede09
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-03-19 16:53:21 +0900

    [Bronze III] Title: 최소, 최대, Time: 172 ms, Memory: 4892 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+    int arr[N];
+    for (int i = 0; i < N; i++){
+        scanf("%d",&arr[i]);
+    }
+    int max = arr[0];
+    int min = arr[0];
+    
+    for (int i = 0; i < N; i++){
+        int curr = arr[i];
+        if (max < curr){
+            max = curr;
+        }
+        if (min > curr){
+            min = curr;
+        }
+    }
+    printf("%d %d",min,max);
+    return 0;
+}
