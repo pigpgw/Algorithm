b964902ee3e02da1d8bba3f1ae57e56b6f657525
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-03-26 21:21:37 +0900

    [Silver III] Title: 1로 만들기, Time: 616 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+dp = [9999] * (N + 1)
+dp[0] = 0
+dp[1] =  0
+
+for i in range(2,N + 1):
+    if i >= 3 and i % 3 == 0:
+        dp[i] = min(dp[i],dp[i // 3] + 1)
+    if i >= 2 and i % 2 == 0:
+        dp[i] = min(dp[i],dp[i // 2] + 1)
+    dp[i] = min(dp[i],dp[i - 1] + 1)
+print(dp[N])
