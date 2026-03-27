5f8f754aeb25d0f7bbc5bd4e55119ac46d6b33e2
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-03-27 14:59:50 +0900

    [Silver I] Title: 카드 구매하기, Time: 168 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,11 @@
+N = int(input())
+value_list = list(map(int, input().split()))
+dp = [0] * (N + 1)
+for i in range(N):
+    dp[i + 1] = value_list[i]
+
+for i in range(2, N + 1):
+    for j in range(1, i):
+        dp[i] = max(dp[i - j] + value_list[j - 1], dp[i])
+
+print(dp[N])
