c9897a62fbd83637adae4e6646700d5a38675760
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-03-29 12:54:24 +0900

    [Silver I] Title: 오르막 수, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,11 @@
+N = int(input())
+dp = [[0] * 10 for i in range(1001)]
+for i in range(10):
+    dp[1][i] = 1
+
+for i in range(2, 1001):
+    for j in range(10):
+        for k in range(j + 1):
+            dp[i][j] += dp[i - 1][k]
+            dp[i][j] %= 10007
+print(sum(dp[N]) % 10007)
