31fbac82f3a9553b4fe0bdb3fde37621fd6b82ef
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-03-31 19:55:22 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -5,31 +5,31 @@ board = []
 for i in range(N):
     board.append(list(map(int, input())))
 
-visited = [[False] * M for i in range(N)]
+visited = [[False] * M for _ in range(N)]
 
-dx = [0, -1, 0, 1]
-dy = [-1, 0, 1, 0]
+directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
 
 def bfs(startY, startX):
     queue = deque()
-    queue.append([startY, startX, 1])
+    queue.append((startY, startX, 1))
     visited[startY][startX] = True
     while queue:
         currY, currX, currCount = queue.popleft()
-        for i in range(4):
-            nx = dx[i] + currX
-            ny = dy[i] + currY
+        if currY == N - 1 and currX == M - 1:
+            return currCount
+        for dy, dx in directions:
+            nx = dx + currX
+            ny = dy + currY
             if nx < 0 or nx >= M or ny < 0 or ny >= N:
                 continue
             if visited[ny][nx]:
                 continue
             if board[ny][nx] == 0:
                 continue
-            if ny == N - 1 and nx == M - 1:
-                return currCount + 1
+
             visited[ny][nx] = True
-            queue.append([ny, nx, currCount + 1])
+            queue.append((ny, nx, currCount + 1))
 
 
 print(bfs(0, 0))
