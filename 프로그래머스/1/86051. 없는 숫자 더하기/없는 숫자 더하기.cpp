4dc09d278cf111e1afc488de136a176f6ceed313
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-05-17 14:38:23 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -5,12 +5,9 @@
 using namespace std;
 
 int solution(vector<int> numbers) {
-    int answer = 0;
-    for (int i = 0; i < 10; i++){
-        auto it = find(numbers.begin(),numbers.end(),i);
-        if (it == numbers.end()){
-            answer += i;
-        }
+    int answer = 45;
+    for (int i = 0; i < numbers.size(); i++){
+        answer -= numbers[i];
     }
     return answer;
 }
