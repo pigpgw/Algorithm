c8131aef073a1445fcb2e6a355cc148b6adaf2be
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-05-17 15:31:01 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -4,15 +4,9 @@
 using namespace std;
 
 string solution(string phone_number) {
-    string answer = "";
-    // 길이에서 4를 뺴고 0부터 대체
     int len = phone_number.size();
-    for (int i = 0; i < len; i++){
-        if (i < len - 4){
-            answer += "*";
-        } else {
-            answer += phone_number[i];
-        }
+    for (int i = 0; i < len - 4; i++){
+        phone_number[i] = '*';
     }
-    return answer;
+    return phone_number;
 }
