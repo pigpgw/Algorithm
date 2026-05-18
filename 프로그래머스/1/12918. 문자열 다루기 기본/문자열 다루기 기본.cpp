a29fcae5e199989fc6340b619b7ba5231d440161
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-05-18 13:53:46 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    int len = s.size();
+    if (len != 4 && len != 6) return false;
+    for (int i = 0; i < len; i++){
+        int asii = s[i] - '0';
+        if (asii < 0 || asii > 9) return false;
+    }
+    return true;
+}
