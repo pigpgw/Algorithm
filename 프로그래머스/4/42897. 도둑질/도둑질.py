417286b6e5551df6c1ba49add45e8463371e101d
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-05-22 00:30:24 +0900

    [level 4] Title: 도둑질, Time: 340.71 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,14 +1,16 @@
 def solution(money):
-    dp1= money[1:]
-    dp1[0] = money[1]
-    dp1[1] = max(money[1], money[2])
-    for i in range(2,len(money) - 1):
-        dp1[i] = max(dp1[i - 2] + money[i + 1],dp1[i - 1])
-
-    dp2= money[:-1]
-    dp2[0] = money[0]
-    dp2[1] = max(money[0], money[1])
-    for i in range(2,len(money) - 1):
-        dp2[i] = max(dp2[i - 2] + money[i],dp2[i - 1])
+    # 첫집을 터냐 안터냐
+    n = len(money)
+    # 첫집 털기
+    dp1 = [0] * n
+    dp1[0] = money[0]
+    dp1[1] = money[0]
+    for i in range(2,n - 1):
+        dp1[i] = max(dp1[i - 2] + money[i],dp1[i - 1])
     
-    return max(dp1[-1],dp2[-1])
+    # 첫집 안털기
+    dp2 = [0] * n
+    dp2[1] = money[1]
+    for i in range(2,n):
+        dp2[i] = max(dp2[i - 2] + money[i],dp2[i - 1])
+    return max(dp1[n - 2],dp2[n - 1])
