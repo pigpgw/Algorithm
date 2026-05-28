88e0e89b87c81a8369e1defa3b6a16eec739295f
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-05-28 15:41:08 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/README.md
+++ 프로그래머스/2/12980. 점프와 순간 이동/README.md
@@ -1,10 +1,10 @@
 # [level 2] 점프와 순간 이동 - 12980 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12980?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12980) 
 
 ### 성능 요약
 
-메모리: 9.17 MB, 시간: 0.03 ms
+메모리: 11.3 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 21일 20:19:09
+2026년 05월 28일 15:41:04
 
 ### 문제 설명
 
--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,12 +1,13 @@
-
-
 def solution(n):
-    bat = 0
+    # 건전지 사용량 최솟값
+    # 점프시 건전지 이동한만큼 사용
+    # 순간이동 : (현재까지 온 거리) x 2 = 건전지 미사용
+    # N이 2의 배수아니면 -1씩 뺴기 2의 배수면 순간이동
+    result = 0
     while n > 0:
-        if n % 2 == 0:
-            n /= 2
+        if n % 2 != 0:
+            n -=1
+            result += 1
         else:
-            n -= 1
-            bat += 1
-    return bat
-            
+            n /= 2
+    return result
