95a927307c8caf8359fccd1d0b2fa7276002de47
pigpgw <133184988+pigpgw@users.noreply.github.com>
2026-07-08 09:52:31 +0900

    [level 2] Title: 구명보트, Time: 8.51 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,15 +1,14 @@
 def solution(people, limit):
-    count = 0
-    people.sort()
-    left = 0
-    right = len(people) - 1
-    while left <= right:
-        if people[left] + people[right] <= limit:
-            left += 1
-        right -= 1
-        count += 1
-    return count
-        
-            
-        
-            
+    answer = 0
+    people.sort(reverse=True) # nlogn
+    lIdx = 0
+    rIdx = len(people) - 1
+    # 시간복잡도 n^2
+    while lIdx <= rIdx: # 몇번돌까 고민하기 pop으로 최대 n번이니 n
+        onePerson = people[lIdx]
+        lIdx += 1
+        secondPerson = people[rIdx]
+        if onePerson + secondPerson <= limit:
+            rIdx -= 1
+        answer += 1
+    return answer
